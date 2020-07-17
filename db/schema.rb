a36661cc2d96143ff8e70599496f15d69e14ebc5
いぬい <inui@inuinoMacBook-Pro.local>
2020-07-17 22:25:16 +0900

    dbの変更

--- db/schema.rb
+++ db/schema.rb
@@ -9,9 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 2020_07_07_180029) do
-
+ActiveRecord::Schema.define(version: 2020_07_16_052754) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -20,8 +18,9 @@ ActiveRecord::Schema.define(version: 2020_07_07_180029) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
