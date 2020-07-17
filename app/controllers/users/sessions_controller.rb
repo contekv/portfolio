899ee024c86db11d1a39bca700894582333f464d
いぬい <inui@inuinoMacBook-Pro.local>
2020-07-17 21:47:12 +0900

    テストユーザーの登録

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,25 +3,28 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  def test_guest
+    @user ||= User.find_by(email: "test@test.com")
+    sign_in @user
+    redirect_to user_path(resource.id), notice: "お試し頂き、ありがとうございます!"
+  end
   # GET /resource/sign_in
   # def new
-  #   super
+  # super
   # end
 
   # POST /resource/sign_in
   # def create
-  #   super
+  # super
   # end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  # def destroy; end
 
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 end
