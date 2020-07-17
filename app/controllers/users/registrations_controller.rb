002b0a5021f94e8a3ffda2035bad0fcdc481d9e9
いぬい <inui@inuinoMacBook-Pro.local>
2020-07-17 22:42:38 +0900

    after_sign_up_path

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
-
   # GET /resource/sign_up
   # def new
-  #   super
+  # super
   # end
 
   # POST /resource
   # def create
-  #   super
+  # super
+
   # end
 
   # GET /resource/edit
@@ -38,22 +36,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def cancel
   #   super
   # end
-
-  # protected
-
+  protected
+    # The path used after sign up.
+    def after_sign_up_path_for(resource)
+      user_path
+    end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
   # end
 
   # The path used after sign up for inactive accounts.
