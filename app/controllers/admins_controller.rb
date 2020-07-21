10292a48c36482371d9c3e6802d964dacbb4fb93
いぬい <inui@inuinoMacBook-Pro.local>
2020-07-21 18:56:50 +0900

    before_action追加

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,11 @@
 class AdminsController < ApplicationController
+  layout 'devise'
+  before_action :correct_admin 
+
   def show
+  end
+
+  def correct_admin
     redirect_to root_path unless current_user&.admin?
-    render layout: "second_layout"
   end
 end
