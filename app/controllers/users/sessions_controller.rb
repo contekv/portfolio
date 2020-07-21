63057f5270545dc40e770809280c08cd607e79b9
いぬい <inui@inuinoMacBook-Pro.local>
2020-07-21 19:07:25 +0900

    インデント修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,5 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
+  
   def test_guest
     @user ||= User.find_by(email: "test@test.com")
     sign_in @user
