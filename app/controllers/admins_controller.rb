ef0aa4a7b516f6ea8739514a894e0112e821756f
いぬい <inui@inuinoMacBook-Pro.local>
2020-08-07 19:04:59 +0900

    path変更

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,7 +2,9 @@ class AdminsController < ApplicationController
   layout "devise"
   before_action :correct_admin
 
-  def show; end
+  def show
+    redirect_to messages_path
+  end
 
   def correct_admin
     redirect_to root_path unless current_user&.admin?
