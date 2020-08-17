eddc1635091a91710197633ff0db81fe1b0c0528
いぬい <inui@inuinoMacBook-Pro.local>
2020-08-17 17:11:43 +0900

    migration

---
+++ db/migrate/20200812075759_add_conver_ref_to_messages.rb
@@ -0,0 +1,5 @@
+class AddConverRefToMessages < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :messages, :conversation, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_101721) do
-
-  create_table "entries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+ActiveRecord::Schema.define(version: 2020_08_12_075759) do
+  create_table "conversations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "room_id", null: false
+    t.bigint "admin_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["room_id"], name: "index_entries_on_room_id"
-    t.index ["user_id"], name: "index_entries_on_user_id"
+    t.index ["admin_id"], name: "index_conversations_on_admin_id"
+    t.index ["user_id"], name: "index_conversations_on_user_id"
   end
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "room_id"
     t.bigint "user_id"
-    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.bigint "conversation_id"
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
-  end
-
-  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -51,8 +44,8 @@ ActiveRecord::Schema.define(version: 2020_08_03_101721) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "entries", "rooms"
-  add_foreign_key "entries", "users"
-  add_foreign_key "messages", "rooms"
+  add_foreign_key "conversations", "users"
+  add_foreign_key "conversations", "users", column: "admin_id"
+  add_foreign_key "messages", "conversations"
   add_foreign_key "messages", "users"
 end
