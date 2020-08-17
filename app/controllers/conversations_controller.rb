220947a9f37a1a0837eff54c269d71c3513a012f
いぬい <inui@inuinoMacBook-Pro.local>
2020-08-17 17:17:36 +0900

    新規controller

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,39 @@
+class ConversationsController < ApplicationController
+  layout "devise"
+  before_action :authenticate_user!
+
+  def create
+    admin = User.find_by(id: 1, name: "管理者")
+    @conversation = Conversation.new(conversation_params.merge(admin_id: admin.id))
+    if @conversation.save
+      redirect_to conversation_path(@conversation)
+    else
+      redirect_back(fallback_location: user_path)
+    end
+  end
+
+  def show
+    if @conversation = Conversation.find(params[:id])
+      user = User.find_by(params[:id])
+      @conversaiton = Conversation.where(user_id: user.id) 
+      @message = Message.new
+      @messages = @conversation.messages
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def index
+    user = User.all
+    @all_conver = Conversation.where(user_id: user)
+    messages = Message.all
+    admin = User.find_by(id:1)
+    not_admin_messages = Message.where.not(conversation_id: admin.id)
+  end
+
+  protected
+
+  def conversation_params
+    params.require(:conversation).permit(:user_id)
+  end
+end
---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,20 @@
+class HomesController < ApplicationController
+  before_action :sign_user
+  before_action :authenticate_user!
+  before_action :correct_user
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  protected
+
+  def sign_user
+    redirect_to root_path unless user_signed_in?
+  end
+
+  def correct_user
+    user = User.find(params[:id])
+    redirect_to root_path unless user == current_user
+  end
+end
