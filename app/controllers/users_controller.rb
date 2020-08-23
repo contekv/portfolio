1587f4af1684e77704f1eedd0f04001af54679ad
いぬい <inui@inuinoMacBook-Pro.local>
2020-08-23 21:35:05 +0900

    controller修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @conversation_id = @user.conversations.pluck(:id)
+    @conversation_id = @user.conversations.first.id
     @conversation = Conversation.new
   end
 end
