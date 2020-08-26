7b048f9c3a33a2b6e325f6c2ba2cdde81c75ac52
いぬい <inui@inuinoMacBook-Pro.local>
2020-08-26 15:33:08 +0900

    admin scope

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,4 +4,5 @@ class Conversation < ApplicationRecord
   validates :user_id, presence: true
   validates :admin_id, presence: true
   scope :sorted_desc, -> { order(created_at: :desc) }
+  scope :admin, -> { find_by(name: "管理者") }
 end
