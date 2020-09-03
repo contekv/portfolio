c89e9c5a553bfe9bae406b0afb102a0f45d28aa8
いぬい <inui@inuinoMacBook-Pro.local>
2020-09-03 16:56:46 +0900

    view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   has_one_attached :avatar
   has_many :conversations, dependent: :destroy
   has_many :messages, dependent: :destroy
+  has_many :medicines,  dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   scope :admin, -> { find_by(name: "管理者") }
   scope :not_admin, -> { where.not(name: "管理者") }
+  scope :sorted_desc, -> { order(created_at: :desc) }
 end
