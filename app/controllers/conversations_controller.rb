ca684f7ab6fbb61855e2ee17621fbf6e10d4921b
いぬい <inui@inuinoMacBook-Pro.local>
2020-09-03 16:54:41 +0900

    controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -25,7 +25,7 @@ class ConversationsController < ApplicationController
 
   def index
     user = User.all
-    @all_convers = Conversation.where(user_id: user)
+    @all_convers = Conversation.where(user_id: user).page(params[:page])
   end
 
   protected
