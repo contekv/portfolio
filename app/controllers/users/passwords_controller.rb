a9985a65ea43f356dfc80226edc560d64b14feba
いぬい <inui@inuinoMacBook-Pro.local>
2020-09-28 12:49:22 +0900

    passwords修正

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -24,30 +24,10 @@ class Users::PasswordsController < Devise::PasswordsController
     resource.reset_password_token = params[:reset_password_token]
   end
 
-  def update
-    self.resource = resource_class.reset_password_by_token(resource_params)
-    yield resource if block_given?
-
-    if resource.errors.empty?
-      if Devise.sign_in_after_reset_password
-        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-        set_flash_message!(:notice, flash_message)
-        resource.after_database_authentication
-        sign_in(resource_name, resource)
-      else
-        set_flash_message!(:notice, :updated_not_active)
-      end
-      respond_with resource, location: after_resetting_password_path_for(resource)
-    else
-      set_minimum_password_length
-      respond_with resource
-    end
-  end
-
   protected
 
     def check_admin
-      if params[:user][:email] == "admin@example.jp" or "test@test.com"
+      if params[:user][:email].in?(["admin@example.jp", "test@test.com"])
         redirect_to root_path, alert: "管理者又はゲストユーザーはパスワードの再発行はできません。"
       end
     end
@@ -56,12 +36,10 @@ class Users::PasswordsController < Devise::PasswordsController
       Devise.sign_in_after_reset_password ? after_sign_in_path_for(resource) : new_session_path(resource_name)
     end
 
-    # The path used after sending reset password instructions
     def after_sending_reset_password_instructions_path_for(resource_name)
       new_session_path(resource_name) if is_navigational_format?
     end
 
-    # Check if a reset_password_token is provided in the request
     def assert_reset_token_passed
       if params[:reset_password_token].blank?
         set_flash_message(:alert, :no_token)
