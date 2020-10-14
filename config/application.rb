593e34ce11d4290ac568c53e5e8ff54a789d716a
いぬい <inui@inuinoMacBook-Pro.local>
2020-10-14 18:06:52 +0900

    generators specs_false

--- config/application.rb
+++ config/application.rb
@@ -10,17 +10,14 @@ module MedicalApp
     config.time_zone = 'Asia/Tokyo'
     config.active_record.default_timezone = :local
     config.action_view.embed_authenticity_token_in_remote_forms = true
-  end
 
-  module Dansyalist
-    class Application < Rails::Application
-      config.i18n.default_locale = :ja
+    module Dansyalist
+      class Application < Rails::Application
+        config.i18n.default_locale = :ja
+      end
     end
-  end
-
-  config.generators do |g| 
-    g.test_framework :rspec,
-      fixtures: false,
+    config.generators do |g|
+      g.test_framework :rspec,
       view_specs: false,
       helper_specs: false,
       routing_specs: false
